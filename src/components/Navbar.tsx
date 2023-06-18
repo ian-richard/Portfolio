bc34b5aee4ffdb6567b8886e41d81b9cd38622d8
Ian <68618201+ian-richard@users.noreply.github.com>
2023-06-18 23:09:27 +0100

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Logo from "../assets/logo.png";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div className="text-[#0a192f] bg-white font-[Pacifico] text-3xl">
+        &nbsp;I.R&nbsp;
+      </div>
+
+      {/* menu */}
+      <ul className="hidden md:flex">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Skills</a>
+        </li>
+        <li>
+          <a href="#">Work</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen  bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <a href="#">Home</a>
+        </li>
+        <li className="py-6 text-4xl">
+          <a href="#">About</a>
+        </li>
+        <li className="py-6 text-4xl">
+          <a href="#">Skills</a>
+        </li>
+        <li className="py-6 text-4xl">
+          <a href="#">Work</a>
+        </li>
+        <li className="py-6 text-4xl">
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+
+      {/* Social Icons */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="#"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-800">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="#"
+            >
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
